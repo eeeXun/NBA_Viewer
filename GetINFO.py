be93806ffe815c72ad4fe1e37cb966ca389d2792
eeeXun <sdes96303@gmail.com>
2021-06-06 10:34:01 +0800

    change the way of "data" initialization

--- GetINFO.py
+++ GetINFO.py
@@ -22,7 +22,7 @@ class Fetch:
         url = "https://www.nba.com/teams"
         self.browser.get(url)
         teams = self.browser.find_elements_by_class_name("TeamFigure_tfMainLink__mH93D.Anchor_external__Mh-vB.Anchor_complexLink__2NtkO")
-        self.data["teams"] = dict.fromkeys([i.text for i in teams])
+        self.data["teams"] = dict.fromkeys([i.text for i in teams], {"teamData": {}, "playerData": {}})
         profiles = self.browser.find_elements_by_link_text("Profile")
         pLinks = [i.get_attribute("href") for i in profiles]
         self.getTeamDatas(pLinks)
@@ -33,7 +33,6 @@ class Fetch:
             tName = " ".join(teamName.text.split())
             teamINFO = self.browser.find_elements_by_class_name("TeamHeader_rankValue__1pj3i")
             dataColumn = ["PPG", "RPG", "APG", "OPPG"]
-            self.data["teams"][tName] = {"teamData": {}}
             for i in range(4):
                 self.data["teams"][tName]["teamData"][dataColumn[i]] = teamINFO[i].text
             print(tName)
