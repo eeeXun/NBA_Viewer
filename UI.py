36793080d4558d09311b4083a7f4d1b385e7a683
eeeXun <sdes96303@gmail.com>
2021-07-02 16:56:11 +0800

    add reload button, but tkinter would be locked when updating

--- UI.py
+++ UI.py
@@ -1,33 +1,31 @@
 from tkinter import *
 from tkhtmlview import HTMLLabel
-import os, json
-import io
+from GetINFO import Fetch
+import json
 
 class MyApp(Tk):
     def __init__(self):
         super().__init__()
-        self.data = self.getData()
         self.setupUI()
-        self.setTeam()
-        self.team=""
 
     def getData(self):
         with open("data.json", "r") as f:
             return json.load(f)
 
     def setupUI(self):
+        self.data = self.getData()
         myFont = "Monospace 13"
         self.title("NBA_Viewer")
         self.teamList = Listbox(self, width=25, height=40, font=myFont)
         self.playerList = Listbox(self, width=25, height=20, font=myFont)
-        self.teamIMG = HTMLLabel(self, width=25, height=20, font=myFont)
+        self.teamIMG = HTMLLabel(self, width=25, height=9, font=myFont)
         self.teamDatas = [Label(self, width=25, font=myFont) for i in range(4)]
         self.updateLB = Label(self, text="Update Time: {}".format(self.data["updateTime"]),
                               font=myFont)
         # Teams label
         Label(self, text="Teams", font=myFont).grid(row=0, column=0)
         # Reload button
-        Button(self, text="Reload", font=myFont).grid(row=6, column=3, sticky="e")
+        Button(self, text="Reload", font=myFont, command=self.reloadData).grid(row=6, column=3, sticky="e")
         # Show player button
         Button(self,text="Show Info",font=myFont,command=self.popOutPlayer).grid(row=6,column=1, columnspan=2)
         self.teamList.grid(row=1, column=0, rowspan=5)
@@ -36,6 +34,17 @@ class MyApp(Tk):
             self.teamDatas[i].grid(row=i+1, column=2)
         self.playerList.grid(row=5, column=1, columnspan=2)
         self.updateLB.grid(row=6)
+        # Set all teams
+        self.setTeam()
+
+    def reloadData(self):
+        # Clear all
+        for item in self.grid_slaves():
+            item.destroy()
+        # Reload data
+        Fetch().start()
+        # Redraw
+        self.setupUI()
 
     def setTeam(self):
         self.teamList.insert(END, *self.data["teams"].keys())
@@ -43,23 +52,22 @@ class MyApp(Tk):
 
     def teamSelected(self, event):
         selection = self.teamList.curselection()
-        team = self.teamList.get(selection)
-        self.team=team
-        self.showTeam(team)
-        self.setPlayer(team)
+        self.team = self.teamList.get(selection)
+        self.showTeam()
+        self.setPlayer()
 
-    def showTeam(self, team):
-        IMG = self.data["teams"][team]["teamData"]["IMG"]
+    def showTeam(self):
+        IMG = self.data["teams"][self.team]["teamData"]["IMG"]
         self.teamIMG.set_html('<img src="{}" width="250" height="200">'.format(IMG))
         dataSet= ["PPG", "RPG", "APG", "OPPG"]
         for i in range(4):
             self.teamDatas[i].config(text=
                                      dataSet[i]
                                      + ": "
-                                     + self.data["teams"][team]["teamData"][dataSet[i]])
+                                     + self.data["teams"][self.team]["teamData"][dataSet[i]])
 
-    def setPlayer(self, team):
-        players = self.data["teams"][team]["playerData"].keys()
+    def setPlayer(self):
+        players = self.data["teams"][self.team]["playerData"].keys()
         self.playerList.delete(0, END)
         self.playerList.insert(END, *players)
 
